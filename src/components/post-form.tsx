489075817acd902ee3f135ed83c9d2ef462b8fe1
Eric Oliveira <ericjmoliveira@gmail.com>
2023-06-13 18:05:27 -0300

    Add edit post feature

--- src/components/post-form.tsx
+++ src/components/post-form.tsx
@@ -4,19 +4,43 @@ import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
 
-export function PostForm({ userId }: { userId: string }) {
-  const [title, setTitle] = useState('');
-  const [subtitle, setSubtitle] = useState('');
-  const [content, setContent] = useState('');
+interface PostFormProps {
+  userId: string;
+  editPostData?: { id: string; title: string; subtitle?: string; content: string };
+}
+
+export function PostForm({ userId, editPostData }: PostFormProps) {
+  const [title, setTitle] = useState(editPostData ? editPostData.title : '');
+  const [subtitle, setSubtitle] = useState(editPostData ? editPostData.subtitle : '');
+  const [content, setContent] = useState(editPostData ? editPostData.content : '');
 
   const router = useRouter();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/api/posts', {
-      method: 'POST',
-      body: JSON.stringify({ userId, title, subtitle: subtitle ? subtitle : undefined, content })
+    const newPostData = { userId, title, subtitle: subtitle ? subtitle : undefined, content };
+
+    if (!editPostData) {
+      const response = await fetch('http://localhost:3000/api/posts', {
+        method: 'POST',
+        body: JSON.stringify(newPostData)
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        return toast(data.error, { type: 'error', theme: 'dark' });
+      }
+
+      toast(data.message, { type: 'success', theme: 'dark' });
+
+      return router.push(`/posts/${data.post.id}/${data.post.slug}`);
+    }
+
+    const response = await fetch(`http://localhost:3000/api/posts/${editPostData.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(newPostData)
     });
 
     const data = await response.json();
@@ -27,7 +51,7 @@ export function PostForm({ userId }: { userId: string }) {
 
     toast(data.message, { type: 'success', theme: 'dark' });
 
-    router.push(`/posts/${data.post.id}/${data.post.slug}`);
+    return router.push(`/posts/${data.post.id}/${data.post.slug}`);
   };
 
   return (
@@ -73,7 +97,7 @@ export function PostForm({ userId }: { userId: string }) {
         />
       </div>
       <button className="rounded-md border border-black bg-black px-4 py-2 font-medium text-white transition hover:border-black hover:bg-white hover:text-black">
-        Create post
+        {editPostData ? 'Save changes' : 'Create post'}
       </button>
     </form>
   );
